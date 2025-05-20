6a5c49fd504b4b2d69ccd5590ea97d526e868382
sachingoyal2022 <121950916+sachingoyal2022@users.noreply.github.com>
2025-05-20 18:52:21 +0530

    Update gcp_sample.tf

--- gcp_sample.tf
+++ gcp_sample.tf
@@ -1,4 +1,4 @@
-resource "google_container_cluster" "positive1" {
+resource "google_container_cluster" "positive11" {
   name               = "marcellus-wallace"
   location           = "us-central1-a"
   initial_node_count = 3
