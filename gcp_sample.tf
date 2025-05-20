76eed4f9a8c369f14b353eff4ad82e0e6cbf8a8a
sachingoyal2022 <121950916+sachingoyal2022@users.noreply.github.com>
2025-05-20 17:26:59 +0530

    Create gcp_sample.tf

---
+++ gcp_sample.tf
@@ -0,0 +1,10 @@
+resource "google_container_cluster" "positive1" {
+  name               = "marcellus-wallace"
+  location           = "us-central1-a"
+  initial_node_count = 3
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+}
